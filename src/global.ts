8cf91f0368b6497f7af740f8e9b21828ec8dc830
ParthSuthar-Technomark <parth.suthar@technomark.io>
2023-11-07 11:59:06 +0530

    Update global.ts

--- src/global.ts
+++ src/global.ts
@@ -6,14 +6,40 @@ export const QboParams = {
     GrantType: "authorization_code",
     GrantTypeForrefreshToken: "refresh_token"
 }
-
 export class qboDetail {
-    code: string 
-    state: string 
+    code: string
+    state: string
     realmId: string
 }
 
 export class TokenInfo {
     code: string
     companyId?:number
+}
+
+export class login {
+    Username: String;
+    Password: String;
+}
+export class ApiResponse{
+    ResponseStatus : string;
+    Message: string;
+    ResponseData: {
+      TwoFactorEnabled: boolean,
+      Token: {
+        Username: string;
+        Token: string;
+        TokenExpiry: string;
+      } | null;
+    } | null;
+    ErrorData: {
+      ErrorCode: string;
+      Error: string;
+      ErrorDetail: string | null;
+    } | null;
+}
+
+export enum Role {
+    Admin = 1,
+    Employee = 2
 }

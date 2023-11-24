f826e624b510681b63ed365567e1ea4cab1d0e42
Manan Navadiya <manan.navadiya@PACIFICABS.COM>
2023-11-24 15:44:05 +0530

    Changes in Login, Home and User

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -7,16 +7,34 @@ import { ApiService } from 'src/app/services/api/api.service';
 import { ApiResponse } from 'src/global';
 import { HttpClient,  HttpResponse } from '@angular/common/http';
 import * as XLSX from 'xlsx';
+import { TableColumn, TableData } from 'src/app/shared/table/table.component';
 
 declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, AfterViewInit {
-  client :any;
-  ClientId : number;
+  data: TableData[] = [];
+  IsClientField: boolean = true;
+  headers: TableColumn[] = [
+    { header: 'Id', field: 'Id' },
+    { header: 'Name', field: 'name' },
+    { header: 'Accoun Type', field: 'accounttype' },
+    { header: 'Account Subtype', field: 'accountsubtype' },
+    { header: 'Date', field: 'Date' },
+    { header: 'Debit', field: 'debit' },
+    { header: 'Credit', field: 'credit' },
+    { header: 'Error', field: 'ErrorDetail' },
+    { header: 'Action', field: 'action' },
+  ];
+
+  form!: FormGroup;
+  client: any;
+  qbobuttons : boolean = false;
+  ClientId: number;
+
   @ViewChild('fileInput') fileInput: ElementRef;
   clientform: FormGroup = new FormGroup({
     Clientname: new FormControl(''),
@@ -27,25 +45,75 @@ export class HomeComponent implements OnInit, AfterViewInit {
     private router: Router,
     private service: ApiService,
     private spinner: NgxSpinnerService,
-    private toastr: ToastrService,
-  ) { }
+    private toastr: ToastrService
+  ) {}
 
   ngAfterViewInit(): void {
     $('[data-bs-toggle="tooltip"]').tooltip();
   }
 
   ngOnInit(): void {
-    this.clientform = this.builder.group(
-      {
-        Clientname: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
-        Year: ['', [Validators.required]],
-        FormType: ['', [Validators.required]],
-      },
-    );
+    
+    this.clientform = this.builder.group({
+      Clientname: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(50),
+        ],
+      ],
+      Year: ['', [Validators.required]],
+      FormType: ['', [Validators.required]],
+    });
+    if(this.data.length > 0 ){
+      this.initializeForm();
+    }
+    this.GetclientDetials();
 
     $('[data-bs-toggle="tooltip"]').tooltip();
   }
 
+  GetclientDetials() {
+    this.spinner.show();
+    this.service.GetClient().subscribe((res:ApiResponse) => {
+      this.spinner.hide();
+      if (res && res.ResponseStatus === 'Success') {
+        console.log(res.ResponseData[0]);
+        
+        if(res.ResponseData[0]?.clientUserMappings != null){
+          console.log(res.ResponseData[0]?.clientUserMappings);
+          this.IsClientField = false
+          this.ClientId = res.ResponseData[0].clientUserMappings?.Id
+          localStorage.setItem("clientId", String (this.ClientId))
+          this.clientform.patchValue({
+            Clientname: res.ResponseData[0].clientUserMappings?.ClientName,
+            Year : res.ResponseData[0].clientUserMappings?.Year,
+            FormType: res.ResponseData[0].clientUserMappings?.Formtype
+          })
+          console.log(this.clientform.value);
+          this.clientform.controls?.['Clientname'].disable();
+          this.clientform.controls?.['Year'].disable();
+          this.clientform.controls?.['FormType'].disable();
+        }
+        if(res.ResponseData[0]?.clientAccountDetail){
+          console.log(res.ResponseData[0]?.clientAccountDetail);
+          
+          this.data = res.ResponseData[0]?.clientAccountDetail
+          if(this.data.length > 0 ) {
+            this.initializeForm();
+          }
+          if(this.data.length === 0) {
+            this.qbobuttons = true
+          }
+        }
+      }
+      else if (res.ResponseStatus === 'Failure') {
+        this.toastr.error(res.ErrorData.Error);
+        console.log("Message", res.Message, "Error", res.ErrorData.Error);
+      }
+    })
+  }
   UploadData() {
     if (this.clientform.valid) {
       this.spinner.show();
@@ -79,12 +147,16 @@ export class HomeComponent implements OnInit, AfterViewInit {
       (response: HttpResponse<ArrayBuffer>) => {
         this.spinner.hide();
         const arrayBuffer = response.body;
-        const workbook: XLSX.WorkBook = XLSX.read(new Uint8Array(arrayBuffer), { type: 'array' });
+        const workbook: XLSX.WorkBook = XLSX.read(new Uint8Array(arrayBuffer), {
+          type: 'array',
+        });
         const sheetName = workbook.SheetNames[0];
         const excelData: XLSX.WorkSheet = workbook.Sheets[sheetName];
         const jsonData = XLSX.utils.sheet_to_json(excelData, { header: 1 });
         console.log(jsonData);
-        const blob = new Blob([arrayBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const blob = new Blob([arrayBuffer], {
+          type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        });
         const link = document.createElement('a');
         link.href = window.URL.createObjectURL(blob);
         link.download = fileName;
@@ -92,45 +164,198 @@ export class HomeComponent implements OnInit, AfterViewInit {
         link.click();
         document.body.removeChild(link);
       },
-      error => {
-        this.toastr.error("Failed to fetch Excel data.");
-        console.error("Error fetching Excel data:", error);
+      (error) => {
+        this.toastr.error('Failed to fetch Excel data.');
+        console.error('Error fetching Excel data:', error);
       }
     );
   }
 
-  uploadedFile:any;
- 
+  uploadedFile: any;
+
   onFileChange(event: any) {
-    console.log("Uploaded file",event.target);
-    
+    console.log('Uploaded file', event.target);
+
     const file = event.target.files[0];
-    this.uploadedFile = file; 
+    this.uploadedFile = file;
   }
 
-  
   uploadFile() {
-    
     if (!this.uploadedFile) {
       this.toastr.error('Please select a file.');
       return;
     }
-  
+
     const formData = new FormData();
     formData.append('file', this.uploadedFile);
-    this.client = localStorage.getItem("clientId")
+    this.client = localStorage.getItem('clientId');
     console.log(this.client);
-    this.service.ImportExcel(formData,this.client).subscribe((response: any) => {
-      this.spinner.hide();
-      if (response && response.ResponseStatus === 'Success') {
-        console.log(response.Message);
-        this.toastr.success('File uploaded successfully');
-        this.fileInput.nativeElement.value = '';
-      } else if (response.ResponseStatus === 'Failure') {
-        this.toastr.error(response.ErrorData.Error);
-        console.log('Message', response.Message, 'Error', response.ErrorData.Error);
-      }
-    }
-    );
+    this.service
+      .ImportExcel(formData, this.client)
+      .subscribe((response: any) => {
+        this.spinner.hide();
+        if (response && response.ResponseStatus === 'Success') {
+          console.log(response.Message);
+          this.data = response.ResponseData 
+          if(this.data.length > 0 ) {
+            this.initializeForm();
+          }
+          if(this.data.length === 0) {
+            this.qbobuttons = true
+          }
+          console.log(response.ResponseData);
+          
+          this.toastr.success('File uploaded successfully');
+          this.fileInput.nativeElement.value = '';
+        } else if (response.ResponseStatus === 'Failure') {
+          this.toastr.error(response.ErrorData.Error);
+          console.log(
+            'Message',
+            response.Message,
+            'Error',
+            response.ErrorData.Error
+          );
+        }
+      });
   }
+
+
+  //#region Qbo Process
+  AddtoQbo(){
+    this.service.AddDataToQbo().subscribe((res:any) => {
+      console.log(res)
+      if (res && res.ResponseStatus === 'Success') {
+        this.toastr.success("Data added successfully");
+      }
+      else if (res.ResponseStatus === 'Failure') {
+        this.toastr.error(res.ErrorData.Error)
+      }
+    })
+  }
+
+  RevrseEntry(){
+    this.service.ReversalEntry().subscribe((res:any) => {
+      console.log(res)
+      if (res && res.ResponseStatus === 'Success') {
+        this.toastr.success("All Account has been Inactivated","Client Has Been Disconnected",);
+      }
+      else if (res.ResponseStatus === 'Failure') {
+        this.toastr.error(res.ErrorData.Error)
+      }
+    })
+  }
+
+  //#endregion
+  //#region  error table
+  initializeForm(): void {
+    const formControls: any = {};
+
+    this.data.forEach((row, rowIndex) => {
+      this.headers.forEach((header, columnIndex) => {
+        let initialValue = row[header.field];
+        if (header.field === 'debit' || header.field === 'credit') {
+          initialValue = parseFloat(initialValue).toFixed(2);
+          console.log(initialValue)
+        }
+          formControls[`${rowIndex}_${header.field}`] = [
+            initialValue,
+            [Validators.required] 
+          ];
+      });
+    });
+
+    this.form = this.builder.group(formControls);
+  }
+
+  getCellControl(rowIndex: number, field: string): any {
+    return this.form.get(`${rowIndex}_${field}`);
+  }
+
+  onCellChange(rowIndex: number, field: string, event: any): void {
+    const control = this.form.get(`${rowIndex}_${field}`);
+    const currentValue = control?.value;
+
+    control?.setValue(event.target.value);
+
+    // Update the data array
+    this.data[rowIndex][field] = event.target.value;
+  }
+
+  onDeleteRow(rowIndex: number): void {
+    console.log(rowIndex);
+    this.data.splice(rowIndex, 1);
+    console.log(this.data);
+    const currentFormValues = this.form.getRawValue();
+    this.headers.forEach((item) => {
+      delete currentFormValues[`${rowIndex}_${item.field}`];
+    });
+    console.log('curruntdata =>', currentFormValues);
+
+    this.initializeForm();
+    console.log(this.form.value);
+  } 
+
+  onSave() {
+    const transformedData: TableData[] = [];
+    this.spinner.show();
+    for (let i = 0; i < this.data.length; i++) {
+      const transformedRow: TableData = {};
+
+      this.headers.forEach((header) => {
+        if (header.field !== 'action' && header.field !== 'ErrorDetail') {
+          const cellValue = this.form.get(`${i}_${header.field}`)?.value;
+          transformedRow[header.field] = cellValue;
+        }
+      });
+
+      transformedData.push(transformedRow);
+    }
+
+    const AccountDetail = {
+      ClientId: this.ClientId,
+      ClientAccountDetail : transformedData
+    }
+
+    if(this.form.valid){
+      this.service.SaveClientAccoutn(AccountDetail).subscribe((response: ApiResponse) => {
+        this.spinner.hide();
+        if (response && response.ResponseStatus === 'Success') {
+          this.data = response.ResponseData 
+          if(this.data.length > 0 ) {
+            this.initializeForm();
+          }
+          if(this.data.length === 0) {
+            this.qbobuttons = true
+          }
+        }
+        else if (response.ResponseStatus === 'Failure') {
+          this.toastr.error(response.ErrorData.Error);
+          console.log("Message", response.Message, "Error", response.ErrorData.Error);
+        }
+      })
+    } else {
+      this.spinner.hide();
+      this.toastr.warning("Please Fill All Field");
+    }
+    console.log(transformedData);
+    
+  }
+
+  isFieldWithError(row: TableData, field: string): boolean {
+    const errorDetail = row['ErrorDetail'];
+
+    if (
+      errorDetail === 'AccountSubType not match' &&
+      field === 'accountsubtype'
+    ) {
+      return true;
+    }
+    if (errorDetail === 'AccountType not match' && field === 'accounttype') {
+      return true;
+    }
+
+    return false;
+  }
+  //#endregion
+
 }
--- src/app/main/setting/users/users.component.ts
+++ src/app/main/setting/users/users.component.ts
@@ -39,18 +39,15 @@ export class UsersComponent implements OnInit {
     { header: 'Status', field: 'IsActive' },
   ];
   PageNo: number;
+  select:any;
+  
+
   options = [
     { label: "Admin", value: Role.Admin },
     { label: "Employee", value: Role.Employee }
   ];
 
-  addUserform: FormGroup = new FormGroup({
-    FirstName: new FormControl(''),
-    LastName: new FormControl(''),
-    Email: new FormControl(''),
-    contactNo: new FormControl(''),
-    role: new FormControl('')
-  });
+  addUserform: FormGroup;
 
   UserDetailform: FormGroup = new FormGroup({
     PageNo: new FormControl(1),
@@ -78,14 +75,14 @@ export class UsersComponent implements OnInit {
 
   ngOnInit(): void {
     this.getCompanies();
-    this.GetUserDetial();
+    this.GetUserDetail();
     this.addUserform = this.builder.group(
       {
         FirstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
         LastName: ['', [Validators.required , Validators.minLength(3), Validators.maxLength(20)]],
         Email: ['', [Validators.required, Validators.email]],
         contactNo: ['', [Validators.required , Validators.minLength(10), Validators.maxLength(10)]],
-        role: ['', [Validators.required]],
+        Role: ['', [Validators.required]],
       },
     );
 
@@ -95,47 +92,44 @@ export class UsersComponent implements OnInit {
 
     this.UserDetailform = this.builder.group(
       {
-        PageNo: [1],
-        PageSize: [5],
-        GlobalSearch: [''],
-        SortColumn: [''],
-        IsDesc: [false],
-        IsActive: [null]
+        PageNo: [],
+        PageSize: [],
+        GlobalSearch: [],
+        SortColumn: [],
+        IsDesc: [],
+        IsActive: []
       },
     );
     console.log(this.UserDetailform.value)
   }
 
 
-  addUser() {
+  AddUserButton() {
     if (this.addUserform.valid) {
       this.spinner.show();
-      console.log("data", this.addUserform)
       this.authService.saveUser(this.addUserform.value).subscribe((response: ApiResponse) => {
         this.spinner.hide();
         if (response && response.ResponseStatus === 'Success') {
-          console.log(response.Message);
           this.toastr.success("User created successful");
           this.closeModal();
-          this.GetUserDetial();
+          this.GetUserDetail();
         }
         else if (response.ResponseStatus === 'Failure') {
           this.toastr.error(response.ErrorData.Error);
-          console.log("Message", response.Message, "Error", response.ErrorData.Error);
           this.openModal();
         }
       })
     } else
-    this.toastr.error("Invalid Form's Value")
+    this.toastr.warning("Invalid Form's Value")
   }
 
-  updateUser() {
+  UpdateUserButton() {
     if (this.addUserform.valid) {
        this.userDetails = { UserId: this.UserId ,  
         Email : this.addUserform.value.Email, 
         FirstName : this.addUserform.value.FirstName,
         LastName : this.addUserform.value.LastName,
-        ContactNo : this.addUserform.value.contactNo
+        ContactNo : this.addUserform.value.contactNo,
       };
       console.log(this.userDetails)
       this.authService.saveUser(this.userDetails).subscribe((response: ApiResponse) => {
@@ -145,21 +139,20 @@ export class UsersComponent implements OnInit {
           console.log(response.Message);
           this.toastr.success("User updated successfully");
           this.closeModal();
-          this.GetUserDetial();
+          this.GetUserDetail();
         } else if (response.ResponseStatus === 'Failure') {
           this.spinner.hide();
           this.toastr.error(response.ErrorData.Error);
           console.log("Message", response.Message, "Error", response.ErrorData.Error);
-          // this.openModal();
         }
       });
     }
     else
-      this.toastr.error("Invalid Form's Value")
+      this.toastr.warning("Invalid Form's Value")
     
   }
 
-  get f(): { [key: string]: AbstractControl } {
+  get abstract(): { [key: string]: AbstractControl } {
     return this.addUserform.controls;
   }
 
@@ -177,7 +170,7 @@ export class UsersComponent implements OnInit {
     this.isEditMode = false;
   }
 
-  GetUserDetial() {
+  GetUserDetail() {
     console.log("data", this.UserDetailform.value)
     this.spinner.show();
     this.authService.GetUserDetails(this.UserDetailform.value).subscribe((response: ApiResponse) => {
@@ -196,7 +189,7 @@ export class UsersComponent implements OnInit {
 
 
   Search() {
-    this.GetUserDetial()
+    this.GetUserDetail()
   }
 
   populateEditForm(user: TableData) {
@@ -207,9 +200,17 @@ export class UsersComponent implements OnInit {
       LastName: user['LastName'] || '',
       Email: user['Email'] || '',
       contactNo: user['ContactNo'] || '',
-      role: user['Role'] || ''
+      Role: user['Role'] 
     });
     this.isEditMode = true;
+    const select = user['Role'];
+    const selectedRole = this.options.find(option => option.value === select);
+    console.log('Selected Role:', selectedRole);
+  
+    if (selectedRole) 
+      this.addUserform.get('Role').setValue(selectedRole.value);
+    else 
+      console.error('Invalid Role:', select);
     this.openModal();
   }
 
@@ -228,7 +229,7 @@ export class UsersComponent implements OnInit {
           this.spinner.hide();
           if (response && response.ResponseStatus === 'Success') {
             this.toastr.success('User deleted successfully');
-            this.GetUserDetial();
+            this.GetUserDetail();
           } else if (response.ResponseStatus === 'Failure') {
             this.toastr.error(response.ErrorData.Error);
           }
@@ -256,7 +257,7 @@ export class UsersComponent implements OnInit {
           if (response && response.ResponseStatus === 'Success') {
             const action = newStatus ? 'Activated' : 'InActivated';
             this.toastr.success(`User ${action} successfully`);
-            this.GetUserDetial();
+            this.GetUserDetail();
           } else if (response.ResponseStatus === 'Failure') {
             this.toastr.error(response.ErrorData.Error);
           }
@@ -270,7 +271,7 @@ export class UsersComponent implements OnInit {
     this.UserDetailform.get('PageSize').setValue(PageSize.pageSize);
     this.PageNo = 1;
     this.UserDetailform.get('PageNo').setValue(1);
-    this.GetUserDetial()
+    this.GetUserDetail()
   }
   onPageChange($event:any){
     console.log($event.pageNo)
@@ -278,7 +279,7 @@ export class UsersComponent implements OnInit {
     this.PageNo = $event.pageNo;
     this.UserDetailform.get('PageNo').setValue($event.pageNo);
     console.log(this.UserDetailform)
-    this.GetUserDetial()
+    this.GetUserDetail()
   }
 
 
@@ -288,7 +289,7 @@ export class UsersComponent implements OnInit {
     this.spinner.hide();
     if (response && response.ResponseStatus === 'Success') {
       this.toastr.success(`Email verifcation link send successfully`);
-      this.GetUserDetial();
+      this.GetUserDetail();
     } else if (response.ResponseStatus === 'Failure') {
       this.toastr.error(response.ErrorData.Error);
     }
@@ -339,7 +340,7 @@ export class UsersComponent implements OnInit {
         } 
         this.closeModalForConnection();
       });
-      this.GetUserDetial();
+      this.GetUserDetail();
     }
   }
 
@@ -418,7 +419,7 @@ export class UsersComponent implements OnInit {
           this.spinner.hide();
           if (response && response.ResponseStatus === 'Success') {
             this.toastr.success("Company Removed Successfully");
-            this.GetUserDetial();
+            this.GetUserDetail();
           } else if (response.ResponseStatus === 'Failure') {
             this.toastr.error(response.ErrorData.Error);
             console.log("Message", response.Message, "Error", response.ErrorData.Error);
